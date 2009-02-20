9f54de872869fc63f5d887e2c1f1209afc3d545f
Robert Sesek <rsesek@bluestatic.org>
2009-02-20 00:32:04 -0500

    Initial version of gitcrement.py

---
+++ gitcrement.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3.0
+"""
+FlashSync
+Copyright (c) 2009, Blue Static <http://www.bluestatic.org>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not,
+see <http://www.gnu.org/licenses/>.
+"""
+
+import os, subprocess, sys
+import sqlite3, time
+from datetime import datetime
+
+def main():
+	if len(sys.argv) < 2:
+		sys.exit("Usage: gitcrement [init|current|next|list]")
+	
+	if sys.argv[1] == "init":
+		init()
+	if sys.argv[1] == "current":
+		current()
+	elif sys.argv[1] == "next":
+		next()
+	elif sys.argv[1] == "list":
+		numlist()
+
+SQL = None
+def db():
+	"""Connects to the database"""
+	global SQL
+	
+	# make sure we're in a git repo
+	if subprocess.getoutput("git st 1>/dev/null"):
+		sys.exit("You are not in a Git repository")
+	
+	if SQL == None:
+		SQL = sqlite3.connect(".gitcrement")
+	
+	return SQL
+
+def init():
+	"""Initializes the current directory for gitcrement"""
+	
+	# check for existing db
+	if os.path.exists(".gitcrement"):
+		sys.exit("A Gitcrement database already exists")
+	
+	# create the database
+	db().execute("""
+		CREATE TABLE numbers
+		(
+			id INTEGER PRIMARY KEY,
+			git_hash VARCHAR(40) NOT NULL,
+			author VARCHAR(50) NOT NULL,
+			datetime INT NOT NULL,
+			parent INT NULL REFERENCES versions(id)
+		);
+	""")
+	db().execute("""
+		CREATE TABLE project
+		(
+			key VARCHAR(20) PRIMARY KEY,
+			value TEXT NULL
+		);
+	""")
+	db().execute("INSERT INTO project VALUES ('schema_version', '1')")
+	db().commit()
+
+def current():
+	"""Returns the current version number"""
+	print(db().execute("SELECT MAX(id) AS current FROM numbers").fetchone()[0])
+
+def next():
+	"""Creates a new build number"""
+	git_hash = subprocess.getoutput("git rev-list -1 HEAD")
+	username = subprocess.getoutput("whoami")
+	cur = db().execute("INSERT INTO numbers VALUES (NULL, ?, ?, ?, NULL)", (git_hash, username, int(time.time())))
+	db().commit()
+
+def numlist():
+	"""Lists all the numbers and who commited them"""
+	numbers = db().execute("SELECT * FROM numbers ORDER BY id DESC").fetchall()
+	for number in numbers:
+		print("%4d: By %s on %s at %s" % (number[0], number[2], datetime.fromtimestamp(number[3]).isoformat(" "), number[1]))
+
+if __name__ == '__main__':
+	main()
