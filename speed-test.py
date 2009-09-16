9d9a06b8310917814ad990ed53d9d3c8c91e62c6
Robert Sesek <rsesek@bluestatic.org>
2009-09-15 22:30:01 -0400

    Add charting to speed-test.py

--- speed-test.py
+++ speed-test.py
@@ -20,6 +20,7 @@ import subprocess
 import sys
 import time
 import tempfile
+import urllib
 
 """File to append results to in CSV format."""
 RESULT_DATA_FILE = os.path.expanduser("~/speed-test-results.txt")
@@ -27,7 +28,24 @@ RESULT_DATA_FILE = os.path.expanduser("~/speed-test-results.txt")
 """The file to try to download, used for testing speed."""
 SPEED_TEST_FILE = "http://ubuntu.media.mit.edu/ubuntu-releases/jaunty/ubuntu-9.04-desktop-i386.iso"
 
+"""The number of recent data trys that should be included in the chart."""
+CHART_CARDINALITY = 50
+
+"""Location to store the chart."""
+CHART_FILE = os.path.expanduser("~/speed-test-chart.png")
+
 ###################################################################
+
+def _ConvertSpeed(speed):
+  """Converts a human-readable speed string (e.g. 2.4 MB/s) into KB/s float values."""
+  unit = speed[len(speed)-4:]
+  rate = speed[0:len(speed)-5]
+  if unit == "MB/s":
+    return str(float(rate)*1024)
+  elif unit == "KB/s":
+    return rate
+  else:
+    raise Exception("Unknown unit %s" % unit)
 
 def RunSpeedTest(work_dir):
   """Downloads the test file and return the results."""
@@ -73,6 +91,47 @@ def UpdateLogFile(results, results_):
       results_.write('"%s",' % r)
   results_.write("\n")
 
+def CreateChart(tuples):
+  """Creates a chart for the most recent reporting data."""
+  # Indexes in the tuple for the columns we care about.
+  idx_date_ = 0
+  idx_speed_ = 2
+  
+  tuples_len = len(tuples)
+  if tuples_len > CHART_CARDINALITY:
+    tuples = tuples[tuples_len-CHART_CARDINALITY : tuples_len]
+    tuples_len = CHART_CARDINALITY
+  height = 300
+  width = (10 * CHART_CARDINALITY) + 20 # Two pixels per every data point for good spacing.
+  # if width > 1000: # Google enforces a max width.
+  width = 1000
+  
+  # De-CSV the tuples.
+  tuples_split = map(lambda t: t.split(','), tuples)
+  
+  data_points = map(lambda t: _ConvertSpeed(t[idx_speed_]), tuples_split)
+  data_points_floats = map(lambda f: float(f), data_points)
+  
+  min_val = min(data_points_floats)
+  max_val = max(data_points_floats)
+  
+  x_values = list()
+  i = 0
+  for tup in tuples_split:
+    if i % 5 == 0:
+      x_values += [datetime.fromtimestamp(float(tup[idx_date_])).strftime("%d/%m %H:%m")]
+    i += 1
+  
+  chart_url = "http://chart.apis.google.com/chart?chtt=%s&cht=lc&chg=2,20&chm=o,0066FF,0,-1.0,6&chs=%dx%d&chd=t:%s&chds=%f,%f&chxt=x,y&chxl=0:|%s|1:|%s|" % (
+    urllib.quote("Speed Test Results (KB/s) by Date"),
+    width, height,
+    ','.join(data_points),
+    min_val-100, max_val+100, # Fuzz the min and max for spacing issues.
+    '|'.join(map(urllib.quote, x_values)),
+    '|'.join((str(min_val-100), str(min_val), str((min_val+max_val)/2), str(max_val), str(max_val+100))),
+  )
+  return chart_url
+
 def Main():
   """Main program body. Runs a speed test and stores the result."""
   output_dir = tempfile.mkdtemp()
@@ -87,6 +146,12 @@ def Main():
   
   results = RunSpeedTest(output_dir)
   UpdateLogFile(results, results_)
+  
+  # Create the chart.
+  results_.seek(0)
+  tuples = results_.readlines()
+  chart_url = CreateChart(tuples[1:])
+  urllib.urlretrieve(chart_url, CHART_FILE)
   
   # Clean the work directory.
   shutil.rmtree(output_dir, True)
