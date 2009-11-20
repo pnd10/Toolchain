347ac41883ce9d6874182bfdc05227656b3eec95
Robert Sesek <rsesek@bluestatic.org>
2009-11-20 00:28:27 -0500

    Add some documentation for tree2graph.py

--- tree2graph.py
+++ tree2graph.py
@@ -1,3 +1,34 @@
+"""
+tree2graph.py
+Copyright (c) 2009, Robert Sesek <http://www.bluestatic.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+Tree2Graph:
+
+This program will create an OmniGraffle 5 Professional document given a
+tree. The tree structure can have an arbitrary number of nodes and should be
+in the format described below.
+
+Usage:
+
+python tree2graph.py < tree.txt > tree.graffle
+./some-binary | python tree2graph.y > tree.graffle
+"""
+
 import plistlib
 import sys
 
